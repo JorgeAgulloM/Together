a07edb517c3944276462b7767fb7afefa3a3c78b
Jorge Agulló <agullojorge@gmail.com>
2022-05-02 12:13:55 +0200

    update gradel before gitignore change

--- app/src/main/java/com/softyorch/together/MainActivity.kt
+++ app/src/main/java/com/softyorch/together/MainActivity.kt
@@ -8,19 +8,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.softyorch.together.ui.theme.TogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +37,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ViewContainer()
-            Greeting("Matrix")
+
         }
     }
 }
@@ -45,7 +54,29 @@ fun ViewContainer() {
 @Composable
 fun Toolbar() {
     TopAppBar(
-        title = { Text(text = "Together", color = colorResource(id = R.color.primal_color)) },
+        title = {
+            Text(
+                text = "Together",
+                color = colorResource(id = R.color.primal_color)
+            )
+
+        },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Rounded.Home,
+                    contentDescription = "Together",
+                    tint = colorResource(R.color.primal_color)
+                )
+            }
+        },
+
+/*        navigationIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_round_houses),
+                contentDescription = ""
+            )
+        },*/
         backgroundColor = colorResource(id = R.color.back_ground)
     )
 }
@@ -53,8 +84,9 @@ fun Toolbar() {
 @Composable
 fun Content() {
 
-    var login by rememberSaveable { mutableStateOf("Text") }
-    var password by rememberSaveable { mutableStateOf("Text") }
+    var login by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val dataOk: Boolean = (login.length >= 3 && password.length >= 8)
 
     Box(
         modifier = Modifier
@@ -77,7 +109,20 @@ fun Content() {
                         onValueChange = {
                             login = it
                         },
-                        label = {
+                        colors = TextFieldDefaults.textFieldColors(
+                            placeholderColor = colorResource(R.color.text_hint),
+                            textColor = colorResource(R.color.text_shadow),
+                            errorCursorColor = Color.Red,
+                            focusedIndicatorColor = colorResource(R.color.action_color),
+                            unfocusedIndicatorColor = colorResource(R.color.text_hint)
+                        ),
+                        shape = RoundedCornerShape(
+                            topStart = 0.dp,
+                            topEnd = 30.dp,
+                            bottomStart = 0.dp,
+                            bottomEnd = 30.dp
+                        ),
+                        placeholder = {
                             Text(text = "Login")
                         }
                     )
@@ -87,10 +132,23 @@ fun Content() {
                         value = password,
                         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                         modifier = Modifier.align(Alignment.Bottom).padding(bottom = 16.dp),
+                        colors = TextFieldDefaults.textFieldColors(
+                            placeholderColor = colorResource(R.color.text_hint),
+                            textColor = colorResource(R.color.text_shadow),
+                            errorCursorColor = Color.Red,
+                            focusedIndicatorColor = colorResource(R.color.action_color),
+                            unfocusedIndicatorColor = colorResource(R.color.text_hint)
+                        ),
+                        shape = RoundedCornerShape(
+                            topStart = 0.dp,
+                            topEnd = 30.dp,
+                            bottomStart = 0.dp,
+                            bottomEnd = 30.dp
+                        ),
                         onValueChange = {
                             password = it
                         },
-                        label = {
+                        placeholder = {
                             Text(text = "Password")
                         }
                     )
@@ -104,27 +162,15 @@ fun Content() {
                     modifier = Modifier
                         .align(Alignment.Bottom),
                     colors = ButtonDefaults.buttonColors(
-                        backgroundColor = colorResource(R.color.back_ground),
-                        contentColor = colorResource(R.color.primal_color)
-                    )
+                        backgroundColor = colorResource(R.color.action_color),
+                        contentColor = colorResource(R.color.text_shadow)
+                    ),
+                    enabled = dataOk
 
                 ) {
                     Text(text = "Login")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TogetherTheme {
-        Greeting("Android")
     }
 }
